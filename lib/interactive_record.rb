fe1479db7eaa213b139df1c8da570ca57b0e28d0
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:27:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ class InteractiveRecord
   end 
   
   def col_names_for_insert 
-    self.class.column_names.delete_if {|col| col == "id"}.join
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end 
   
   def values_for_insert 
