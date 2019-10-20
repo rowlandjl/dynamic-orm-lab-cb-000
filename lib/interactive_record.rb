216616c7b3bdee358a405bdaa02e7b4a9c4178e1
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:19:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
   def self.column_names 
     DB[:conn].results_as_hash = true 
     
-    sql = "PRAGMA table_info('#{table_info}')"
+    sql = "PRAGMA table_info('#{table_name}')"
     
     table_info = DB[:conn].execute(sql)
     column_names = []
