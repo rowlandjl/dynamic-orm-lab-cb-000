17269baa012f8181e84d32892c1d7ba85880428c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:29:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,8 @@ class InteractiveRecord
   def values_for_insert 
     values = []
     
-    self.class.column_names.each do |col|
+    self.class.column_names.each do |col_name|
+      
   end 
   
   def save 
