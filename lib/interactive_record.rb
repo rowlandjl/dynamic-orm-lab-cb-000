b26570e549d696a79dff6e0bad1f048d4c51cc2d
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:37:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,7 +26,7 @@ class InteractiveRecord
     attr_accessor col_name.to_sym
   end 
   
-  def initialize(options={}) 
+  def initialize(attributes) 
     # options.each do |key, value|
     #   self.send("#{key}=", value)
     # end 
