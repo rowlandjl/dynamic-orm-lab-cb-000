f1f8aaf1eea381cbc7e5abdd621b1f4af50b7d25
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:29:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -44,7 +44,7 @@ class InteractiveRecord
     values = []
     
     self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'"
+      values << "'#{send(col_name)}'" unless 
   end 
   
   def save 
