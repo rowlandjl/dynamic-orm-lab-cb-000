0478552d1a7bb59e1e7d7dffd0a9985dd2907602
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:46:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,7 +46,7 @@ class InteractiveRecord
   end 
   
   def save 
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert)", values_for_insert)
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})", values_for_insert)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
