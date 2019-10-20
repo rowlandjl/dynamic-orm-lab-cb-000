3c8533c500dc53474aac34f7620998e73a62ce9d
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 18:32:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end 
   
   def save 
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} ()")
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})")
   end 
   
   def self.find_by_name 
